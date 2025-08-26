48234fdb35e08bbb676674c813767b426b90b8c0
Amir Hossain Limon <146566739+Amirhossainlimon@users.noreply.github.com>
2025-08-27 01:04:05 +0600

    Add files via upload

---
+++ input.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+main(){
+  String?name;
+  print("Enter your name: ");
+  name = stdin.readLineSync()!;
+  int?age;
+  print("Enter your age: ");
+  age = int.parse(stdin.readLineSync()!);
+
+  print("Name: $name");
+  print("Age: $age");
+
+
+
+
+}
