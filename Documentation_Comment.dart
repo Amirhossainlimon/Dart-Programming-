7333303a6a3216c1e3d4535fadad010ef3d4b834
Amir Hossain Limon <146566739+Amirhossainlimon@users.noreply.github.com>
2026-06-10 23:39:03 +0600

    Add files via upload

---
+++ Documentation_Comment.dart
@@ -0,0 +1,14 @@
+/// This function adds two numbers.
+int add(int a, int b) {
+  return a + b;
+}
+
+// This function Substraction two numbers.
+int sub(int a, int b) {
+  return a - b;
+}
+void main() {
+  print(add(10, 20));
+
+   print(sub(40, 20));
+}
